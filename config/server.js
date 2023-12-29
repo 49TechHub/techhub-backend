3bbcf275e85ed87b7472b2b9d63cbce6718b137e
Dyvan <dyvan.nozhat@gmail.com>
2023-12-29 12:15:27 +0300

    added proxy to server config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
+  host: env("HOST", "localhost"),
   port: env.int("PORT", 1337),
+  proxy: {
+    enabled: env("PROXY_ENABLED", true),
+    host: env("PROXY_HOST", "localhost"), // Should match the server_name in your Nginx config
+    port: env.int("PROXY_PORT", 80), // Should match the port in your Nginx config
+    ssl: env("PROXY_SSL", true), // Set to true if using HTTPS
+  },
   app: {
     keys: env.array("APP_KEYS"),
   },
